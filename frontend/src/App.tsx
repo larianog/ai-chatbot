e81910231074d627daef5736866bc2a75fb7e59d
Larissa Albuquerque <larissaanogueira24@gmail.com>
2025-11-08 21:49:08 +0100

    Add chat layout on front-end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { apiKey, userId, userName, userToken } from './lib/constants'
+import { useState, useEffect } from 'react';
+import type { User, Channel as StreamChannel } from 'stream-chat';
+import { useCreateChatClient, Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import 'stream-chat-react/dist/css/v2/index.css';
+import './layout.css';
+
+const user: User = {
+  id: userId,
+  name: userName,
+  image: `https://getstream.io/random_png/?name=${userName}`,
+};
+
+const App = () => {
+  const [channel, setChannel] = useState<StreamChannel>();
+  const client = useCreateChatClient({
+    apiKey,
+    tokenOrProvider: userToken,
+    userData: user,
+  });
+
+  useEffect(() => {
+    if (!client) return;
+
+    const channel = client.channel('messaging', 'custom_channel_id', {
+      image: 'https://getstream.io/random_png/?name=react',
+      name: 'Talk about React',
+      members: [userId],
+    });
+
+    setChannel(channel);
+  }, [client]);
+
+  if (!client) return <div>Setting up client & connection...</div>;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Chat client={client}>
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+};
 
-export default App
+export default App;
