37d19105e373b15185a339b3e2fd36cc1f40551a
Larissa Albuquerque <63756257+larianog@users.noreply.github.com>
2025-11-12 07:15:26 -0300

    Add chat (#1)
    * Create a backend folder
    * Add frontend app, with channel and channel list
    * Update Readme
    * Change page title name

---
+++ frontend/src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const apiKey = import.meta.env.VITE_STREAM_API_KEY as string;
+export const userId = import.meta.env.VITE_STREAM_USER_ID as string;
+export const userName = import.meta.env.VITE_STREAM_USER_NAME as string;
+export const userToken = import.meta.env.VITE_STREAM_USER_TOKEN as string;
