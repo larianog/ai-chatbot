6a08dc8679e14abb11c9b73ffc402601f3e7c7d1
Larissa Albuquerque <larissaanogueira24@gmail.com>
2025-11-12 11:11:58 +0100

    Add chanel list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
 import { apiKey, userId, userName, userToken } from './lib/constants'
-import { useState, useEffect } from 'react';
-import type { User, Channel as StreamChannel } from 'stream-chat';
-import { useCreateChatClient, Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+import type { User, ChannelSort, ChannelFilters, ChannelOptions } from 'stream-chat';
+import {
+  useCreateChatClient,
+  Chat,
+  Channel,
+  ChannelHeader,
+  ChannelList,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from 'stream-chat-react';
 
 import 'stream-chat-react/dist/css/v2/index.css';
 import './layout.css';
@@ -12,31 +21,28 @@ const user: User = {
   image: `https://getstream.io/random_png/?name=${userName}`,
 };
 
+const sort: ChannelSort = { last_message_at: -1 };
+const filters: ChannelFilters = {
+  type: 'messaging',
+  members: { $in: [userId] },
+};
+const options: ChannelOptions = {
+  limit: 10,
+};
+
 const App = () => {
-  const [channel, setChannel] = useState<StreamChannel>();
   const client = useCreateChatClient({
     apiKey,
     tokenOrProvider: userToken,
     userData: user,
   });
 
-  useEffect(() => {
-    if (!client) return;
-
-    const channel = client.channel('messaging', 'custom_channel_id', {
-      image: 'https://getstream.io/random_png/?name=react',
-      name: 'Talk about React',
-      members: [userId],
-    });
-
-    setChannel(channel);
-  }, [client]);
-
   if (!client) return <div>Setting up client & connection...</div>;
 
   return (
-    <Chat client={client}>
-      <Channel channel={channel}>
+    <Chat client={client} theme="str-chat__theme-dark">
+      <ChannelList filters={filters} sort={sort} options={options} />
+      <Channel>
         <Window>
           <ChannelHeader />
           <MessageList />
